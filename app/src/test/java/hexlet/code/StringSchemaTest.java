4b89b6242b05688215457e501d62cf15cc48a16d
rostex <rostex2@yandex.ru>
2024-10-24 13:28:55 +0300

    Добавить тест в класс NumberSchemaTest.java

--- app/src/test/java/hexlet/code/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -11,19 +11,26 @@ public class StringSchemaTest {
     StringSchema schema = v.string();
 
     @Test
-    public void testSchema() {
-        assertTrue(schema.isValid("")); // true
-        assertTrue(schema.isValid(null)); // true
+    public void testSchema1() {
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid(null));
 
         schema.required();
 
-        assertFalse(schema.isValid(null)); // false
-        assertFalse(schema.isValid("")); // false
-        assertTrue(schema.isValid("what does the fox say")); // true
-        assertTrue(schema.isValid("hexlet")); // true
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(""));
+        assertTrue(schema.isValid("what does the fox say"));
+        assertTrue(schema.isValid("hexlet"));
 
-        assertTrue(schema.contains("wh").isValid("what does the fox say")); // true
-        assertTrue(schema.contains("what").isValid("what does the fox say")); // true
-        //assertFalse(schema.contains("whatthe").isValid("what does the fox say"));
+        assertTrue(schema.contains("wh").isValid("what does the fox say"));
+        assertTrue(schema.contains("what").isValid("what does the fox say"));
+        assertFalse(schema.contains("whatthe").isValid("what does the fox say"));
+        assertFalse(schema.isValid("what does the fox say"));
+    }
+
+    @Test
+    public void testSchema2() {
+        var schema1 = v.string();
+        assertTrue(schema1.minLength(10).minLength(4).isValid("Hexlet"));
     }
 }
