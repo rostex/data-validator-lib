a18a3f47e29653d50f68176ec2e11f39c98f2cb7
rostex <rostex2@yandex.ru>
2024-10-26 00:13:30 +0300

    Исправить класс StringSchema.java
    Метод required вынесен в классы наследники из-за ошибки компиляции по причине неверного определения типов данных

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -3,6 +3,11 @@ package hexlet.code.schemas;
 
 public class StringSchema extends BaseSchema<String> {
 
+    public StringSchema required() {
+        super.isRequired = true;
+        return this;
+    }
+
     public StringSchema minLength(int lengthValue) {
         super.rules.clear();
         super.rules.add(string -> string.length() >= lengthValue);
