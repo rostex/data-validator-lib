e1f5c98c096f3b1f17552d5a041411f63846b017
rostex <rostex2@yandex.ru>
2024-10-25 23:45:10 +0300

    Обновить класс NumberSchema.java
    Убрать неиспользованные переменные.
    Убрать наследуемые методы isValid и required.
    Обновить методы positive и range с учетом добавления условий в List<Predicate>.

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,38 +1,15 @@
 package hexlet.code.schemas;
 
 public class NumberSchema extends BaseSchema<Integer> {
-    public boolean isRequired = false;
-    private boolean isPositive = false;
-    private Integer startNumber = null;
-    private Integer endNumber = null;
-
-    public NumberSchema required() {
-        this.isRequired = true;
-        return this;
-    }
 
     public NumberSchema positive() {
-        this.isPositive = true;
+        super.rules.add(number -> number > 0);
         return this;
-
     }
 
     public NumberSchema range(int startNumberValue, int endNumberValue) {
-        this.startNumber = startNumberValue;
-        this.endNumber = endNumberValue;
+        super.rules.add(number -> number >= startNumberValue && number <= endNumberValue);
         return this;
     }
 
-
-    @Override
-    public boolean isValid(Integer number) {
-        if (number == null) {
-            return !isRequired;
-        }
-        if (number <= 0) {
-            return !isPositive;
-        }
-        return (startNumber == null || number >= startNumber)
-                && (endNumber == null || number <= endNumber);
-    }
 }
