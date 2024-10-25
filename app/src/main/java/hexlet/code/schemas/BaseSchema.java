dc2f97d68636a0b64543eb133bd661060fca4d6c
rostex <rostex2@yandex.ru>
2024-10-25 23:41:36 +0300

    Обновить класс BaseSchema.java
    Инициализировать List<Predicate> для хранения списка условий.
    Унифицировать метод isValid для работы со всеми наследниками класса

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,8 +1,27 @@
 package hexlet.code.schemas;
 
-public abstract class BaseSchema<T> {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class BaseSchema<T> {
+    public List<Predicate<T>> rules = new ArrayList<>();
+    public boolean isRequired = false;
+
+    public BaseSchema<T> required() {
+        this.isRequired = true;
+        return this;
+    }
 
     public boolean isValid(T value) {
+        if (value == null || value.toString().isEmpty()) {
+            return !isRequired;
+        }
+        for (var rule : rules) {
+            if (!rule.test(value)) {
+                return false;
+            }
+        }
         return true;
     }
 
