dbf89d95e2237ddcfdc47a3620b0fedc17385310
rostex <rostex2@yandex.ru>
2024-10-25 23:44:37 +0300

    Обновить класс MapSchema.java
    Убрать неиспользованные переменные.
    Убрать наследуемые методы isValid и required.
    Обновить методы sizeof и shape с учетом добавления условий в List<Predicate>.

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,49 +3,22 @@ package hexlet.code.schemas;
 import java.util.Map;
 
 public class MapSchema extends BaseSchema<Map<String, String>> {
-    public boolean isRequired = false;
-    private Integer size = null;
-    private Map<String, BaseSchema<String>> schemas;
-
-    public MapSchema required() {
-        this.isRequired = true;
-        return this;
-    }
 
     public MapSchema sizeof(int sizeValue) {
-        this.size = sizeValue;
+        super.rules.add(map -> map.size() >= sizeValue);
         return this;
-
     }
 
     public MapSchema shape(Map<String, BaseSchema<String>> mapSchemas) {
-        this.schemas = mapSchemas;
+        super.rules.add(map ->
+                mapSchemas.entrySet().stream()
+                        .allMatch(entry -> {
+                            String key = entry.getKey();
+                            BaseSchema<String> schema = entry.getValue();
+                            String value = map.get(key);
+                            return schema.isValid(value);
+                        })
+        );
         return this;
-    }
-
-    @Override
-    public boolean isValid(Map<String, String> map) {
-        if (map == null) {
-            return !isRequired;
-        }
-        if (map.isEmpty()) {
-            return isRequired;
-        }
-        if (size != null && map.size() < size) {
-            return false;
-        }
-        if (schemas == null) {
-            return true;
-        }
-        for (var entry : map.entrySet()) {
-            var schemaKey = schemas.get(entry.getKey());
-            if (entry.getValue() == null) {
-                return false;
-            }
-            if (schemaKey != null && !schemaKey.isValid(entry.getValue())) {
-                return false;
-            }
-        }
-        return true;
     }
 }
